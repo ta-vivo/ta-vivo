b11950d3d2cc39dae5c98a347021e899377083da
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-12 15:45:51 -0400

    feat
    * Added check router

--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import userRoutes from '../routes/User';
 import TaskRoutes from '../routes/Task';
+import CheckRoutes from '../routes/Check';
 // Initialization
 let router = express.Router();
 
 // Routes
 router.use('/users', userRoutes);
 router.use('/tasks', TaskRoutes);
+router.use('/checks', CheckRoutes);
 
 export default router;
