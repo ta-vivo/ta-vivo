baf6c417ee0890a8c70f9409852bd026d5a14c03
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-12 16:04:07 -0400

    feat
    * Added models

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -1,9 +1,13 @@
 import User from './User';
 import Checks from './Checks';
 import CheckLogs from './CheckLogs';
+import Integration from './Integration';
+import CheckNotificationMethod from './CheckNotificationMethod';
 
 export {
   User,
   Checks,
   CheckLogs,
+  Integration,
+  CheckNotificationMethod,
 };
