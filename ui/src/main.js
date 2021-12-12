78a9c5e71141ead4cfff14b8def54af5bf5ef2a9
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-12 15:06:32 -0400

    feat
    * Added i18n

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,6 +1,18 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
+
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App).use(router)
+import en from './locales/en.js'
+
+const i18n = createI18n({
+  locale: 'en', // set locale
+  fallbackLocale: 'en', // set fallback locale
+  messages : {
+    en
+  },
+})
+
+const app = createApp(App).use(i18n).use(router)
 app.mount('#app')
