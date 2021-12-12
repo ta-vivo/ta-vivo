fd14f3a30a1bb942440dcd6ff7d716db3ff195b8
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-12 16:21:51 -0400

    feat
    * Added auth routes

--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import userRoutes from '../routes/User';
 import TaskRoutes from '../routes/Task';
 import CheckRoutes from '../routes/Check';
+import Auth from '../routes/Auth';
 // Initialization
 let router = express.Router();
 
@@ -9,5 +10,6 @@ let router = express.Router();
 router.use('/users', userRoutes);
 router.use('/tasks', TaskRoutes);
 router.use('/checks', CheckRoutes);
+router.use('/auth', Auth);
 
 export default router;
