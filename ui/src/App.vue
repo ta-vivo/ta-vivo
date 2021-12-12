0adb6e1077f5ef221abf4e45c4d9e4ddf8efa987
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-11 20:06:32 -0400

    feat
    * Added test data

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,17 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img src="./assets/logo.png" />
+  <div>
+    <p>
+      If Element Plus is successfully added to this project, you'll see an
+      <code v-text="'<el-button>'"></code>
+      below
+    </p>
+    <el-card class="box-card">
+      <template #header>
+        <div class="card-header">
+          <span>Card name</span>
+          <el-button class="button" type="text">Operation button</el-button>
+        </div>
+      </template>
+      <div v-for="o in 4" :key="o" class="text item">
+        {{ 'List item ' + o }}
+      </div>
+    </el-card>
+    <el-button @click="dialogVisible = true" type="primary"
+      >el-button</el-button
+    >
+    <el-dialog
+      v-model="dialogVisible"
+      title="Tips"
+      width="30%"
+      :before-close="handleClose"
+    >
+      <span>This is a message</span>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="dialogVisible = false">Cancel</el-button>
+          <el-button type="primary" @click="dialogVisible = false"
+            >Confirm</el-button
+          >
+        </span>
+      </template>
+    </el-dialog>
+  </div>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import { ElMessageBox } from 'element-plus';
+import { ElDialog, ElCard, ElButton } from 'element-plus';
 
+import { ref } from 'vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    ElDialog,
+    ElCard,
+    ElButton
+  },
+  setup() {
+    const dialogVisible = ref(false);
+    const handleClose = (done) => {
+      ElMessageBox.confirm('Are you sure to close this dialog?')
+        .then(() => {
+          done();
+        })
+        .catch(() => {
+          // catch error
+        });
+    };
+    return {
+      dialogVisible,
+      handleClose,
+    };
+  },
+};
 </script>
 
 <style>
