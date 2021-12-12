a809036d76cd7db469a0713898f594d686b1bbd8
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-12 15:46:07 -0400

    feat
    * Created check model

---
+++ api/src/models/Checks.js
@@ -0,0 +1,30 @@
+import Sequelize from 'sequelize';
+import { sequelize } from '../database/database';
+import { User } from './index';
+
+const Checks = sequelize.define('checks', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  target: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  periodToCheck: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  enabled: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: true
+  },
+
+}, {});
+
+Checks.belongsTo(User);
+User.hasMany(Checks);
+
+export default Checks;
