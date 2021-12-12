fa3a5c411b028c11303cc190b7ac0ca947c76e26
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-11 20:07:34 -0400

    feat
    * Added vue router

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,77 +1,10 @@
 <template>
-  <img src="./assets/logo.png" />
-  <div>
-    <p>
-      If Element Plus is successfully added to this project, you'll see an
-      <code v-text="'<el-button>'"></code>
-      below
-    </p>
-    <el-card class="box-card">
-      <template #header>
-        <div class="card-header">
-          <span>Card name</span>
-          <el-button class="button" type="text">Operation button</el-button>
-        </div>
-      </template>
-      <div v-for="o in 4" :key="o" class="text item">
-        {{ 'List item ' + o }}
-      </div>
-    </el-card>
-    <el-button @click="dialogVisible = true" type="primary"
-      >el-button</el-button
-    >
-    <el-dialog
-      v-model="dialogVisible"
-      title="Tips"
-      width="30%"
-      :before-close="handleClose"
-    >
-      <span>This is a message</span>
-      <template #footer>
-        <span class="dialog-footer">
-          <el-button @click="dialogVisible = false">Cancel</el-button>
-          <el-button type="primary" @click="dialogVisible = false"
-            >Confirm</el-button
-          >
-        </span>
-      </template>
-    </el-dialog>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <router-view/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue';
-import { ElMessageBox } from 'element-plus';
-import { ElDialog, ElCard, ElButton } from 'element-plus';
-
-import { ref } from 'vue';
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    ElDialog,
-    ElCard,
-    ElButton
-  },
-  setup() {
-    const dialogVisible = ref(false);
-    const handleClose = (done) => {
-      ElMessageBox.confirm('Are you sure to close this dialog?')
-        .then(() => {
-          done();
-        })
-        .catch(() => {
-          // catch error
-        });
-    };
-    return {
-      dialogVisible,
-      handleClose,
-    };
-  },
-};
-</script>
 
 <style>
 #app {
@@ -80,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- ui/src/main.js
+++ ui/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-const app = createApp(App)
+const app = createApp(App).use(router)
 app.mount('#app')
