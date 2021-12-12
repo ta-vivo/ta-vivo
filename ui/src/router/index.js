7490602f4962267ff6d70d8c638455c245039caf
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-12 14:48:24 -0400

    feat
    * Created login route

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -8,6 +8,17 @@ const routes = [
     component: Home
   },
   {
+    path: '/auth',
+    component: () => import('../layouts/Auth'),
+    children: [
+      {
+        path: 'login',
+        name: 'Login',
+        component: () => import('../views/Auth/Login.vue')
+      },
+    ]
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
