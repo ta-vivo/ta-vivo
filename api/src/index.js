318c9d43615c4a83986a870a01bae7ac8d428a02
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-13 19:51:45 -0400

    feat
    * Added mandatory data on app started

--- api/src/index.js
+++ api/src/index.js
@@ -1,8 +1,10 @@
 import app from './app';
 import '@babel/polyfill';
+import { MandatoryData } from './database/MandatoryData';
 
-async function main(){
+async function main() {
   await app.listen(process.env.PORT);
+  MandatoryData();
   console.log(`Server running: http://localhost:${process.env.PORT} ✅`);
 }
 
