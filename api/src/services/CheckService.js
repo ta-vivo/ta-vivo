d7ceb2decb89aa5e6732de7413846f16f2c66e12
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-14 00:23:08 -0400

    feat
    * Integrated JWT

--- api/src/services/CheckService.js
+++ api/src/services/CheckService.js
@@ -1,20 +1,36 @@
-import {Check} from '../models/';
+import { Checks } from '../models/';
 
 class CheckService {
 
-  static async create(newUser) {
+  static async create(newCheck) {
+    const checkForCreate = {
+      target: newCheck.target,
+      periodToCheck: newCheck.periodToCheck,
+      enabled: newCheck.enabled ? newCheck.enabled : false,
+      userId: newCheck.user.userId
+    };
+
     try {
-      const entityCreated = await Check.create(newUser);
+      const entityCreated = await Checks.create(checkForCreate);
       return entityCreated;
     } catch (error) {
       throw error;
     }
   }
 
-  static async update(id, user) {
+  static async update(id, check) {
     try {
-      await Check.update(user, { where: { id: id } });
-      return await this.getById(id);
+      const checkForUpdate = {
+        target: check.target,
+        periodToCheck: check.periodToCheck,
+        enabled: check.enabled ? check.enabled : false
+      };
+      // eslint-disable-next-line no-prototype-builtins
+      if (check.hasOwnProperty('enabled')) {
+        checkForUpdate.enabled = check.enabled;
+      }
+      await Checks.update(checkForUpdate, { where: { id: id } });
+      return await this.getById({id, user: check.user});
     } catch (error) {
       throw error;
     }
@@ -24,27 +40,27 @@ class CheckService {
     const { criterions } = params;
 
     try {
-      const { rows } = await Check.findAndCountAll({ ...criterions });
+      const { rows } = await Checks.findAndCountAll({ ...criterions });
       return { rows, count: rows.length };
     } catch (error) {
       throw error;
     }
   }
 
-  static async getById(id) {
+  static async getById({id, user}) {
 
     try {
-      const check = await Check.findOne({ where: { id } });
+      const check = await Checks.findOne({ where: { id, userId: user.userId } });
       return check;
     } catch (error) {
       throw error;
     }
   }
 
-  static async delete(id) {
+  static async delete({id, user}) {
     try {
-      const rowCount = await Check.destroy({
-        where: { id }
+      const rowCount = await Checks.destroy({
+        where: { id, userId: user.userId }
       });
       return { count: rowCount };
     } catch (error) {
