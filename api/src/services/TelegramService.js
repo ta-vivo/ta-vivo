123a6600314e423959492eb0bbadc087f86ab102
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-15 21:41:27 -0400

    feat
    * Created telegram service

---
+++ api/src/services/TelegramService.js
@@ -0,0 +1,26 @@
+
+import TelegramBot from 'node-telegram-bot-api';
+import { PendingIntegration } from '../models';
+
+class TelegramService {
+
+  static listenMessages() {
+    console.log('👂 Listening messages...');
+    const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
+
+    bot.on('message', async (msg) => {
+      if (msg.text === '/start') {
+        const uniqueCode = Math.random().toString(36).substring(2, 7);
+        await PendingIntegration.create({
+          uniqueCode,
+          integrationType: 'telegram',
+          appUserId: msg.from.id
+        });
+        bot.sendMessage(msg.chat.id, `Your unique code is: ${uniqueCode}`);
+      }
+    });
+  }
+
+}
+
+export default TelegramService;
