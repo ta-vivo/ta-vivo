8f18e02f4fd23cf21e23f608a4adfb27a91d72d4
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-15 21:39:44 -0400

    feat
    * Added listener for telegram when app started

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,12 @@
 import app from './app';
 import '@babel/polyfill';
 import { MandatoryData } from './database/MandatoryData';
+import TelegramService from './services/TelegramService';
 
 async function main() {
   await app.listen(process.env.PORT);
   MandatoryData();
+  TelegramService.listenMessages();
   console.log(`Server running: http://localhost:${process.env.PORT} ✅`);
 }
 
