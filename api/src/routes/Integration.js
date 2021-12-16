90198b2bab6b3991a484872ba2e9fa28b8d22d41
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-16 00:29:52 -0400

    feat
    * Created route for fetch all integrations

--- api/src/routes/Integration.js
+++ api/src/routes/Integration.js
@@ -4,6 +4,7 @@ import { verifyToken } from '../middlewares/Auth';
 
 const router = Router();
 
+router.get('/', verifyToken, IntegrationController.getAll);
 router.post('/', verifyToken, IntegrationController.create);
 
 export default router;
