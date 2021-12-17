884af5b0ffef50b6686d64b85364fe82f86e4a9b
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-16 22:22:13 -0400

    feat
    * Added i18n text

--- ui/src/i18n/en-US/index.js
+++ ui/src/i18n/en-US/index.js
@@ -2,6 +2,9 @@
 // so you can safely delete all default props below
 
 export default {
-  failed: 'Action failed',
-  success: 'Action was successful'
+  common: {
+    login: 'Login',
+    username: 'Username',
+    password: 'Password',
+  }
 }
