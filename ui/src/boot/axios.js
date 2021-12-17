6db18f92d9e4ef32809a45cf3c67e470d5904b8b
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-16 22:22:05 -0400

    feat
    * Created axios config

--- ui/src/boot/axios.js
+++ ui/src/boot/axios.js
@@ -1,24 +1,41 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
-// Be careful when using SSR for cross-request state pollution
-// due to creating a Singleton instance here;
-// If any client changes this (global) instance, it might be a
-// good idea to move this instance creation inside of the
-// "export default () => {}" function below (which runs individually
-// for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' })
-
-export default boot(({ app }) => {
-  // for use inside Vue files (Options API) through this.$axios and this.$api
-
-  app.config.globalProperties.$axios = axios
-  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
-  //       so you won't necessarily have to import axios in each vue file
-
-  app.config.globalProperties.$api = api
-  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
-  //       so you can easily perform requests against your app's API
+// We create our own axios instance and set a custom base URL.
+// Note that if we wouldn't set any config here we do not need
+// a named export, as we could just `import axios from 'axios'`
+const axiosInstance = axios.create({
+  baseURL: process.env.API
 })
 
-export { api }
+const axiosInstanceForAuth = axios.create({
+  baseURL: process.env.API
+})
+
+const token = () => 'Bearer ' + window.localStorage.getItem('token')
+
+const authInterceptor = (config) => {
+  config.headers.Authorization = token()
+  return config
+}
+
+axiosInstance.interceptors.request.use(authInterceptor)
+
+// Add a response interceptor
+axiosInstance.interceptors.response.use(response => {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response
+}, function (error) {
+  if (error.response.status === 401) {
+    window.localStorage.removeItem('token')
+    location.reload()
+  }
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  return Promise.reject(error)
+})
+
+// Here we define a named export
+// that we can later use inside .js files:
+export { axiosInstance, axiosInstanceForAuth }
