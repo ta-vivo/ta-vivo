64e667fa882cbb19890707c01f1f6becd48b8568
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-16 23:54:42 -0400

    feat
    * Added route

--- ui/src/layouts/MainLayout.vue
+++ ui/src/layouts/MainLayout.vue
@@ -2,7 +2,7 @@
   <q-layout view="lHh Lpr lFf">
     <q-header class="bg-white text-black" elevated>
       <q-toolbar>
-        <q-btn color="primary" stretch flat icon="eva-activity-outline" :label="$t('common.checks')"></q-btn>
+        <q-btn color="primary" to="/checks" stretch flat icon="eva-activity-outline" :label="$t('common.checks')"></q-btn>
         <q-btn color="primary" stretch flat icon="eva-briefcase-outline" :label="$t('common.integrations')"></q-btn>
       </q-toolbar>
     </q-header>
