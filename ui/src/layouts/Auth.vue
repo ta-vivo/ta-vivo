ae88e6ffbceff330679883c0b84ebaf68956bf88
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-16 22:22:31 -0400

    feat
    * Created auth layout

---
+++ ui/src/layouts/Auth.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AuthLayout",
+});
+</script>
