70f700f8706f09a23e92da662d377b9c9e44b2fa
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-16 22:00:31 -0400

    feat
    * Added notify plugin
    * Added history mode to router

--- ui/quasar.conf.js
+++ ui/quasar.conf.js
@@ -47,7 +47,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
       // publicPath: '/',
@@ -98,7 +98,9 @@ module.exports = configure(function (ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Notify'
+      ]
     },
 
     // animations: 'all', // --- includes all animations
