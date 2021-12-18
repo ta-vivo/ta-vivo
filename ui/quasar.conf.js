f56abfffda87ac1dc8c9c1cd23f24d5691ce9ec3
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-18 14:41:41 -0400

    feat
    * Added icon set to quasar framework

--- ui/quasar.conf.js
+++ ui/quasar.conf.js
@@ -87,7 +87,7 @@ module.exports = configure(function (ctx) {
     framework: {
       config: {},
 
-      // iconSet: 'material-icons', // Quasar icon set
+      iconSet: 'eva-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
 
       // For special cases outside of where the auto-import strategy can have an impact
