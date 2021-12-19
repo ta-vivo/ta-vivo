2fc90b7453dc8ec7869ebd6db58a0b5d2b0eab02
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-19 14:50:48 -0400

    feat
    * Created action to fetch check by id

--- ui/src/store/Checks/actions.js
+++ ui/src/store/Checks/actions.js
@@ -4,6 +4,10 @@ export function fetchAll(context) {
   return axiosInstance.get('/checks?limit=100')
 }
 
+export function fetch(context, payload) {
+  return axiosInstance.get(`/checks/${payload.id}`)
+}
+
 export function create(context, payload) {
   return axiosInstance.post('/checks', payload)
 }
