692ca1ad34e7b9f5d8022b3c33dec692411c79fc
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-19 22:51:16 -0400

    feat
    * Created action to create integrations

--- ui/src/store/Integrations/actions.js
+++ ui/src/store/Integrations/actions.js
@@ -3,3 +3,7 @@ import { axiosInstance } from 'boot/axios'
 export function fetchAll(context) {
   return axiosInstance.get('/integrations')
 }
+
+export function create (context, payload) {
+  return axiosInstance.post('/integrations', payload)
+}
