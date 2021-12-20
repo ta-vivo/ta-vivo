7a6d10704af75abe973264ab6d4359064a43e611
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-19 22:54:18 -0400

    feat
    * Created integration telegram form

---
+++ ui/src/components/Integrations/Form/Telegram.vue
@@ -0,0 +1,62 @@
+<template>
+  <q-form @submit="onSubmit" class="q-gutter-md">
+    <div>
+      <p class="text-bold">{{ $t("common.basicInformation") }}</p>
+    </div>
+    <q-input
+      type="text"
+      outlined
+      v-model="integration.name"
+      :label="$t('common.name')"
+      lazy-rules
+      :rules="[
+        (val) => (val && val.length > 0) || $t('messages.errors.requireField'),
+      ]"
+    />
+    <q-input
+      type="text"
+      hint="Unique code get via Telegram"
+      outlined
+      v-model="integration.uniqueCode"
+      :label="$t('common.uniqueCode')"
+      lazy-rules
+      :rules="[
+        (val) => (val && val.length > 0) || $t('messages.errors.requireField'),
+      ]"
+    />
+    <div class="text-center">
+      <q-btn
+        push
+        :loading="loading"
+        :label="$t('action.save')"
+        type="submit"
+        color="primary"
+      />
+    </div>
+  </q-form>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "ComponentTelegramForm",
+  props: {
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props, { emit }) {
+    const integration = ref({
+      name: "",
+      uniqueCode: "",
+    });
+    return {
+      integration,
+      onSubmit() {
+        emit("saved", integration.value);
+      },
+    };
+  },
+};
+</script>
