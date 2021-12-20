aa56f3e2dc8cfbaf98f8f94f1d9b548e89ea8cb9
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-19 21:04:47 -0400

    feat
    * Added integration route to menu

--- ui/src/layouts/MainLayout.vue
+++ ui/src/layouts/MainLayout.vue
@@ -3,7 +3,7 @@
     <q-header class="bg-white text-black" elevated>
       <q-toolbar>
         <q-btn color="primary" to="/checks" stretch flat icon="eva-activity-outline" :label="$t('common.checks')"></q-btn>
-        <q-btn color="primary" stretch flat icon="eva-briefcase-outline" :label="$t('common.integrations')"></q-btn>
+        <q-btn color="primary" to="/integrations" stretch flat icon="eva-briefcase-outline" :label="$t('common.integrations')"></q-btn>
       </q-toolbar>
     </q-header>
     <q-page-container>
