477415088df71fa0987aa0e2a3f54bf5a7f43eb5
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-19 22:54:30 -0400

    feat
    * Created page to add integrations

---
+++ ui/src/pages/Integrations/Add.vue
@@ -0,0 +1,128 @@
+<template>
+  <q-page padding>
+    <q-card flat bordered>
+      <q-card-section>
+        <p class="text-h5">{{ $t("action.addIntegration") }}</p>
+        <q-btn
+          v-if="selectedIntegration"
+          flat
+          color="primary"
+          :label="$t('action.backToTheList')"
+          icon="eva-arrow-back-outline"
+          @click="selectedIntegration = null"
+        />
+      </q-card-section>
+      <q-card-section v-if="!selectedIntegration">
+        <q-list bordered>
+          <q-item
+            v-for="integration in availableIntegrations"
+            :key="integration.name"
+          >
+            <q-item-section avatar>
+              <q-btn
+                round
+                flat
+                :color="integration.color"
+                :icon="integration.icon"
+              />
+            </q-item-section>
+
+            <q-item-section class="text-capitalize">{{
+              integration.name
+            }}</q-item-section>
+            <q-item-section side>
+              <q-btn
+                push
+                color="primary"
+                :label="$t('action.addIntegration')"
+                icon="eva-plus"
+                @click="selectedIntegration = integration.name"
+              />
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-card-section>
+      <q-card-section v-if="selectedIntegration">
+        <TelegramForm
+          v-if="selectedIntegration === 'telegram'"
+          :loading="loading"
+          @saved="onSubmit"
+        />
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import { useQuasar } from "quasar";
+import { ref } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+import { fabTelegram } from "@quasar/extras/fontawesome-v5";
+import { useI18n } from "vue-i18n";
+import TelegramForm from "components/Integrations/Form/Telegram";
+
+export default {
+  name: "PageAddIntegration",
+  components: {
+    TelegramForm,
+  },
+  setup() {
+    const $q = useQuasar();
+    const $store = useStore();
+    const $router = useRouter();
+    const $t = useI18n().t;
+
+    const integration = ref({
+      name: "",
+      uniqueCode: "",
+    });
+    const loading = ref(false);
+    const availableIntegrations = ref([
+      {
+        name: "telegram",
+        icon: fabTelegram,
+        color: "blue",
+      },
+    ]);
+    const selectedIntegration = ref(null);
+
+    return {
+      integration,
+      loading,
+      availableIntegrations,
+      selectedIntegration,
+      onSubmit(newIntegration) {
+        loading.value = true;
+        $store
+          .dispatch("integrations/create", newIntegration)
+          .then(() => {
+            $q.notify({
+              message: $t("action.integrationCreated"),
+              color: "positive",
+            });
+            $router.push("/integrations");
+          })
+          .catch((error) => {
+            $q.notify({
+              color: "negative",
+              message: error.response.data.message,
+              icon: fabTelegram,
+            });
+          })
+          .finally(() => {
+            loading.value = false;
+          });
+      },
+      getIntegrationIcon(integration) {
+        switch (integration) {
+          case "telegram":
+            return { icon: fabTelegram, color: "blue" };
+          default:
+            return "";
+        }
+      },
+    };
+  },
+};
+</script>
