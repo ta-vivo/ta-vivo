6a8b0302c45aaf0f9fdf3ad8b26096fb097b988d
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-21 19:20:31 -0400

    feat
    * Added logout function
    * Added user information in the header

--- ui/src/layouts/MainLayout.vue
+++ ui/src/layouts/MainLayout.vue
@@ -2,8 +2,39 @@
   <q-layout view="lHh Lpr lFf">
     <q-header class="bg-white text-black" bordered>
       <q-toolbar class="constrain-width">
-        <q-btn color="primary" to="/checks" stretch flat icon="eva-activity-outline" :label="$t('common.checks')"></q-btn>
-        <q-btn color="primary" to="/integrations" stretch flat icon="eva-briefcase-outline" :label="$t('common.integrations')"></q-btn>
+        <q-btn
+          color="primary"
+          to="/checks"
+          stretch
+          flat
+          icon="eva-activity-outline"
+          :label="$t('common.checks')"
+        ></q-btn>
+        <q-btn
+          color="primary"
+          to="/integrations"
+          stretch
+          flat
+          icon="eva-briefcase-outline"
+          :label="$t('common.integrations')"
+        ></q-btn>
+        <q-space />
+        <q-btn-dropdown
+          flat
+          color="primary"
+          :label="$store.getters['auth/getUser'].username"
+        >
+          <q-list>
+            <q-item clickable v-close-popup @click="logout">
+              <q-item-section>
+                <q-item-label class="text-negative">
+                  <q-icon name="eva-log-out-outline" />
+                  Logout
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
       </q-toolbar>
     </q-header>
     <q-page-container>
@@ -13,11 +44,27 @@
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
+import { defineComponent } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
 
 export default defineComponent({
   name: "MainLayout",
 
-  setup() {},
+  setup() {
+    const $store = useStore();
+    const $router = useRouter();
+
+    return {
+      logout () {
+        window.localStorage.removeItem("token");
+        $store.commit("auth/SET_USER", {
+          username: '',
+          id: null,
+        });
+        $router.push("/login");
+      },
+    }
+  },
 });
 </script>
