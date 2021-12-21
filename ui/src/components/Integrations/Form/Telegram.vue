2496825893fffb6e2d2d5bef6becac2a089f0a2f
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-21 00:02:47 -0400

    feat
    * Added instructions to integrate telegram

--- ui/src/components/Integrations/Form/Telegram.vue
+++ ui/src/components/Integrations/Form/Telegram.vue
@@ -1,39 +1,69 @@
 <template>
-  <q-form @submit="onSubmit" class="q-gutter-md">
-    <div>
-      <p class="text-bold">{{ $t("common.basicInformation") }}</p>
+  <div class="row q-col-gutter-md">
+    <div class="col-xs-12 col-md-5">
+      <q-form @submit="onSubmit" class="q-gutter-md">
+        <div>
+          <p class="text-bold">{{ $t("common.basicInformation") }}</p>
+        </div>
+        <q-input
+          type="text"
+          :hint="$t('messages.information.telegramIntegrationNamePlaceholder')"
+          outlined
+          v-model="integration.name"
+          :label="$t('common.name')"
+          lazy-rules
+          :rules="[
+            (val) =>
+              (val && val.length > 0) || $t('messages.errors.requireField'),
+          ]"
+        />
+        <q-input
+          type="text"
+          :hint="$t('messages.information.uniqueCodeGetFromTelegram')"
+          outlined
+          v-model="integration.uniqueCode"
+          :label="$t('common.uniqueCode')"
+          lazy-rules
+          :rules="[
+            (val) =>
+              (val && val.length > 0) || $t('messages.errors.requireField'),
+          ]"
+        />
+        <div class="text-center">
+          <q-btn
+            push
+            :loading="loading"
+            :label="$t('action.save')"
+            type="submit"
+            color="primary"
+          />
+        </div>
+      </q-form>
     </div>
-    <q-input
-      type="text"
-      outlined
-      v-model="integration.name"
-      :label="$t('common.name')"
-      lazy-rules
-      :rules="[
-        (val) => (val && val.length > 0) || $t('messages.errors.requireField'),
-      ]"
-    />
-    <q-input
-      type="text"
-      :hint="$t('messages.information.uniqueCodeGetFromTelegram')"
-      outlined
-      v-model="integration.uniqueCode"
-      :label="$t('common.uniqueCode')"
-      lazy-rules
-      :rules="[
-        (val) => (val && val.length > 0) || $t('messages.errors.requireField'),
-      ]"
-    />
-    <div class="text-center">
-      <q-btn
-        push
-        :loading="loading"
-        :label="$t('action.save')"
-        type="submit"
-        color="primary"
-      />
+    <div class="col-xs-12 col-md-6">
+      <p class="text-bold">Telegram</p>
+      <p>{{ $t("messages.information.telegramSetupInformation") }}</p>
+      <q-separator class="q-mb-md" />
+      <p>
+        <span class="text-bold">1-</span> {{ $t("messages.information.telegramSetupBot") }}
+        <a
+          class="text-primary text-bold"
+          href="https://t.me/TaVivoBot"
+          target="_blank"
+          >@ta-vivo-bot</a
+        >
+      </p>
+      <p>
+        <span class="text-bold">2-</span> {{ $t("messages.information.telegramSetupBotCommand") }}
+        <q-img
+          class="q-mt-md"
+          src="~assets/telegram/telegram-bot-command.jpeg"
+          spinner-color="white"
+          style="max-width: 80%"
+        />
+      </p>
     </div>
-  </q-form>
+  </div>
 </template>
 
 <script>
