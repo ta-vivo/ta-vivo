5a12318de60f9ffb09088d54135dc9dab88fd26b
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-21 19:19:47 -0400

    feat
    * Decode JWT if this exists for add user information to store

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -2,9 +2,20 @@
   <router-view class="constrain-width" />
 </template>
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
+import jwtDecode from "jwt-decode";
 
 export default defineComponent({
-  name: 'App'
-})
+  name: "App",
+  created() {
+    const token = window.localStorage.getItem("token");
+    if (token) {
+      const decoded = jwtDecode(token);
+      this.$store.commit("auth/SET_USER", {
+        username: decoded.username,
+        id: decoded.id,
+      });
+    }
+  },
+});
 </script>
