db5b6190e57531be384056d8d19dc9cddd696a0c
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-29 20:56:21 -0400

    feat
    * Move cron time table to separate file

--- api/src/services/CheckService.js
+++ api/src/services/CheckService.js
@@ -3,18 +3,8 @@ import cron from 'cron';
 import { Checks, CheckLogs, CheckIntegration, Integration } from '../models/';
 import TelegramService from './TelegramService';
 import { Op } from 'sequelize';
+import cronTimeTable from '../utils/cronTimeList';
 
-const cronTimeTable = [
-  { label: '10s', value: '*/10 * * * * *' },
-  { label: '30s', value: '*/30 * * * * *' },
-  { label: '1m', value: '* * * * *' },
-  { label: '5m', value: '*/5 * * * *' },
-  { label: '10m', value: '*/10 * * * *' },
-  { label: '30m', value: '*/30 * * * *' },
-  { label: '1h', value: '* */1 * * *' },
-  { label: '2h', value: '* */2 * * *' },
-  { label: '12h', value: '* */12 * * *' },
-];
 
 let cronJobs = {};
 
---
+++ api/src/utils/cronTimeList.js
@@ -0,0 +1,11 @@
+export default [
+  { label: '10s', value: '*/10 * * * * *' },
+  { label: '30s', value: '*/30 * * * * *' },
+  { label: '1m', value: '* * * * *' },
+  { label: '5m', value: '*/5 * * * *' },
+  { label: '10m', value: '*/10 * * * *' },
+  { label: '30m', value: '*/30 * * * *' },
+  { label: '1h', value: '* */1 * * *' },
+  { label: '2h', value: '* */2 * * *' },
+  { label: '12h', value: '* */12 * * *' },
+];
