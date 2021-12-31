dfc35da836facfcd9f972b8442fe5f437a6a43fc
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-30 23:24:27 -0400

    feat
    * Added dashboard route to main router

--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -4,6 +4,8 @@ import TaskRoutes from '../routes/Task';
 import CheckRoutes from '../routes/Check';
 import IntegrationRoutes from '../routes/Integration';
 import Auth from '../routes/Auth';
+import Dashboard from '../routes/Dashboard';
+
 // Initialization
 let router = express.Router();
 
@@ -13,5 +15,6 @@ router.use('/tasks', TaskRoutes);
 router.use('/checks', CheckRoutes);
 router.use('/auth', Auth);
 router.use('/integrations', IntegrationRoutes);
+router.use('/dashboard', Dashboard);
 
 export default router;
