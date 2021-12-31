ae25cfc888f358dd20dec98b2d6001e44fefd74f
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-30 23:25:00 -0400

    feat
    * Created dashbaord card to use in dashboard

---
+++ ui/src/components/Widgets/DashboardCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-card flat bordered style="max-width:200px">
+    <q-card-section class="text-center">
+      <q-avatar
+        size="50px"
+        :color="color"
+        text-color="white"
+        :icon="icon"
+      />
+    </q-card-section>
+    <q-card-section>
+      <p class="text-h6">{{ title }}</p>
+      <p class="text-h4">{{ value }}</p>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'ComponentDashboardCard',
+  props: {
+    icon: {
+      type: String,
+      default: '',
+    },
+    color: {
+      type: String,
+      default: 'primary',
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+    value: {
+      type: Number,
+      default: 0,
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
