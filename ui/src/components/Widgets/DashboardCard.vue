a18c355b98c9f3bb10907ab957ecfea6cc59c088
Albert Hidalgo <alhidalgodev@gmail.com>
2021-12-31 00:32:49 -0400

    feat
    * Added loading

--- ui/src/components/Widgets/DashboardCard.vue
+++ ui/src/components/Widgets/DashboardCard.vue
@@ -1,39 +1,39 @@
 <template>
-  <q-card flat bordered style="max-width:200px">
+  <q-card flat bordered style="max-width: 200px">
     <q-card-section class="text-center">
-      <q-avatar
-        size="50px"
-        :color="color"
-        text-color="white"
-        :icon="icon"
-      />
+      <q-avatar size="50px" :color="color" text-color="white" :icon="icon" />
     </q-card-section>
     <q-card-section>
       <p class="text-h6">{{ title }}</p>
       <p class="text-h4">{{ value }}</p>
     </q-card-section>
+    <q-inner-loading :showing="loading" label-style="font-size: 1.1em" />
   </q-card>
 </template>
 
 <script>
 export default {
-  name: 'ComponentDashboardCard',
+  name: "ComponentDashboardCard",
   props: {
     icon: {
       type: String,
-      default: '',
+      default: "",
     },
     color: {
       type: String,
-      default: 'primary',
+      default: "primary",
     },
     title: {
       type: String,
-      default: '',
+      default: "",
     },
     value: {
       type: Number,
       default: 0,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
     },
   },
   setup() {
