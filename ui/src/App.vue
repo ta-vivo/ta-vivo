461b66c85fd1040468e21beb72f16a3828c79bc6
Albert Hidalgo <alhidalgodev@gmail.com>
2022-01-28 18:35:58 -0400

    feat
    * Added footer component to the app

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <router-view class="constrain-width" />
+  <footer-component />
 </template>
 <script>
 import { defineComponent } from "vue";
 import jwtDecode from "jwt-decode";
+import FooterComponent from 'components/Interface/Footer';
 
 export default defineComponent({
   name: "App",
+  components: {
+    FooterComponent
+  },
   created() {
     const token = window.localStorage.getItem("token");
     if (token) {
