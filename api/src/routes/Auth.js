0d6d507a8e69da5fc4445bac67071ae9e0545e8d
Albert Hidalgo <alhidalgodev@gmail.com>
2022-02-05 18:49:22 -0400

    removed monorepo

--- api/src/routes/Auth.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import AuthController from '../controllers/AuthController';
-
-const router = Router();
-
-router.post('/login', AuthController.login);
-
-export default router;
