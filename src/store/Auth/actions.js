200221fa311e18c61ab2f03922ae96c0c6da08c3
Albert Hidalgo <alhidalgodev@gmail.com>
2022-02-15 22:48:17 -0400

    feat
    * Created register action

--- src/store/Auth/actions.js
+++ src/store/Auth/actions.js
@@ -3,3 +3,7 @@ import { axiosInstanceForAuth } from 'boot/axios'
 export function login(context, { email, password }) {
   return axiosInstanceForAuth.post('/auth/login', { email, password })
 }
+
+export function register(context, { email, password, confirmPassword, fullname }) {
+  return axiosInstanceForAuth.post('/auth/register', { email, password, confirmPassword, fullname })
+}
