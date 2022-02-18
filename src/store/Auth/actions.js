fbd781541b2f97f0244e52a07f3fd127acc67253
Albert Hidalgo <alhidalgodev@gmail.com>
2022-02-17 22:47:07 -0400

    feat
    * Created action to register email

--- src/store/Auth/actions.js
+++ src/store/Auth/actions.js
@@ -1,4 +1,4 @@
-import { axiosInstanceForAuth } from 'boot/axios'
+import { axiosInstanceForAuth, axiosInstance } from 'boot/axios'
 
 export function login(context, { email, password }) {
   return axiosInstanceForAuth.post('/auth/login', { email, password })
@@ -6,4 +6,8 @@ export function login(context, { email, password }) {
 
 export function register(context, { email, password, confirmPassword, fullname }) {
   return axiosInstanceForAuth.post('/auth/register', { email, password, confirmPassword, fullname })
+}
+
+export function registerEmailConfirmation(context, { uniqueCode }) {
+  return axiosInstance.post('/auth/register-email-confirmation', { uniqueCode })
 }
