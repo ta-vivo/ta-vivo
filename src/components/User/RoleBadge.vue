e07c2f9b8fba6486b9953931a7ec008281a2d8c6
Albert Hidalgo <alhidalgodev@gmail.com>
2022-03-25 22:44:29 -0400

    feat
    * Added badge to set the user role as subscription in the top bar menu

---
+++ src/components/User/RoleBadge.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-badge
+    outline
+    :color="roleSettings.backgroundColor"
+    :text-color="roleSettings.textColor"
+    :label="roleSettings.role"
+  />
+</template>
+
+<script>
+export default {
+  name: "ComponentUserBadge",
+  props: {
+    role: String
+  },
+  setup(props) {
+    const roleSettings = {
+      textColor: "white",
+      backgroundColor: "primary",
+      role: "Basic",
+    };
+
+    // switch (props.role.toLowerCase()) {
+    switch ('enterprise+') {
+      case "basic":
+        roleSettings.backgroundColor = "black";
+        roleSettings.role = "Basic";
+        break;
+      case "pro":
+        roleSettings.backgroundColor = "primary";
+        roleSettings.role = "Pro";
+        break;
+      case "enterprise":
+        roleSettings.backgroundColor = "orange";
+        roleSettings.role = "Enterprise";
+        break;
+      case "enterprise+":
+        roleSettings.backgroundColor = "purple";
+        roleSettings.role = "Enterprise+";
+        break;
+      default:
+        roleSettings.textColor = "black";
+        roleSettings.backgroundColor = "primary";
+    }
+    return {
+      roleSettings,
+    };
+  },
+};
+</script>
