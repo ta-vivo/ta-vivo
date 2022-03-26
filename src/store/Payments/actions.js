3cdbece66f045f6f6f685085a01ff6069ff57414
Albert Hidalgo <alhidalgodev@gmail.com>
2022-03-25 20:51:56 -0400

    feat
    * Created action to save subscription

--- src/store/Payments/actions.js
+++ src/store/Payments/actions.js
@@ -1,5 +1,9 @@
 import { axiosInstance } from 'boot/axios';
 
-export function fetchPricing (context) {
+export function fetchPricing(context) {
   return axiosInstance.get('/pricing')
+}
+
+export function saveSubscription(context, { subscriptionId }) {
+  return axiosInstance.post('payments/paypal-subscription', { subscriptionId })
 }
