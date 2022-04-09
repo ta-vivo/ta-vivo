f2bc3d07c3aac2229b7a01c7b41e9d959aaefc70
Albert Hidalgo <alhidalgodev@gmail.com>
2022-04-09 11:21:36 -0400

    feat
    * Added privacy policy

--- src/pages/Legal/PrivacyPolicy.vue
+++ src/pages/Legal/PrivacyPolicy.vue
@@ -1,11 +1,215 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <p class="text-h3 text-center">{{ $t("common.privacyPolicy") }}</p>
+    <p>
+      {{ systemUrl }} (a service by {{ appName }}). We are committed to ensuring
+      that your privacy is protected. Therefore, if you provide us with personal
+      information, you can be assured that it will only be used in accordance
+      with this Privacy Policy.
+    </p>
+    <p class="text-h5">What Does This Policy Cover?</p>
+    <p>
+      This Privacy Policy applies to information that we collect about you when
+      you use out website {{ systemUrl }} also, throughout this Privacy Policy,
+      we'll refer to our website and ping endpoints collectively as “Services”.
+    </p>
+    <p>
+      Below we explain how we collect, use, and share information about you,
+      along with the choices you have regarding that information.
+    </p>
+    <p class="text-h5">Information We Collect</p>
+    <p>
+      We only collect information about you if we have a reason to do so–for
+      example, to provide our Services, to communicate with you, or to make our
+      Services better.
+    </p>
+    <p>
+      We collect information in two ways, if and when you provide information to
+      us and automatically through operating our Services.
+    </p>
+    <p class="text-bold">Information You Provide to Us</p>
+    <p>
+      We collect the information you provide directly to us. Here are the types
+      of information we collect:
+    </p>
+    <p>
+      <span class="text-bold">Basic Account Information:</span> We ask for basic
+      information from you to set up your account. We require individuals who
+      sign up for a {{ systemUrl }} account to provide an email address, full
+      name, and password and that's it.
+    </p>
+    <p>
+      <span class="text-bold">Transaction and Billing Information:</span> If you
+      buy a subscription from us–you will provide additional personal and
+      payment information required to process the transaction and your payment,
+      such as your name, and contact information. This information is stored in
+      Paypal NOT in {{ appName }}.
+    </p>
+    <p>
+      <span class="text-bold">Credentials for Notification Services:</span>
+      Depending on the notification methods you configure on {{ appName }}, you
+      may provide us with additional email addresses, phone numbers, Discord,
+      Slack, Telegram or credentials for posting to your chat rooms and to your
+      incident management systems.
+    </p>
+    <p>
+      <span class="text-bold">Communicating with Us:</span> You may also provide
+      us information when you communicate with our team about a support
+      question.
+    </p>
+    <p class="text-bold">Information That We Collect Automatically</p>
+    <p>We also collect some information automatically:</p>
+    <p>
+      <span class="text-bold">Log Information:</span> Like most online service
+      providers, we collect information that web browsers, mobile devices, and
+      servers typically provide, such as the browser type, IP address, unique
+      device identifiers, language preference, referring site, the date and time
+      of access and operating system. We collect log information when you use
+      our Services–for example when you create or make changes to your account.
+    </p>
+    <p class="text-h5">How and Why We Use Information</p>
+    <p>
+      We use information about you as mentioned above and for the purposes
+      listed below:
+    </p>
+    <p>
+      To provide our Services–for example, to set up and maintain your account
+      or charge you for any of our paid Services;
+    </p>
+    <p>
+      To further develop and improve our Services–for example by adding new
+      features we think our users will enjoy or will help them to use our
+      Services more efficiently;
+    </p>
+    <p>
+      To communicate with you, for example, to solicit your feedback, or to keep
+      you up to date on {{ appName }} and our products.
+    </p>
+    <p class="text-bold">Legal Bases for Collecting and Using Information</p>
+    <p>
+      A note here for those in the European Union about our legal grounds for
+      processing information about you under EU data protection laws. Our use of
+      your information is based on the grounds that:
+    </p>
+    <ol>
+      <li>
+        The use is necessary to fulfill our commitments to you under our Terms
+        of Service or other agreements with you or is necessary to administer
+        your account–for example, to enable access to our website on your device
+        or charge you for a paid plan; or
+      </li>
+      <li>The use is necessary for compliance with a legal obligation; or</li>
+      <li>
+        The use is necessary to protect your vital interests or those of another
+        person; or
+      </li>
+      <li>
+        We have a legitimate interest in using your information–for example, to
+        provide and update our Services, to improve our Services so we can offer
+        you an even better user experience, to safeguard our Services, to
+        communicate with you, to monitor and prevent any problems with our
+        Services, and to personalize your experience.
+      </li>
+    </ol>
+    <p class="text-h5">Sharing Information</p>
+    <p>We do not sell our users' private personal information.</p>
+    <p>
+      We share information about you in the limited circumstances spelled out
+      below and with appropriate safeguards on your privacy:
+    </p>
+    <ul>
+      <li>
+        Subsidiaries, Employees, and Independent Contractors: We may disclose
+        information about you to our subsidiaries, our employees, and
+        individuals who are our independent contractors that need to know the
+        information to help us provide our Services or to process the
+        information on our behalf. We require our subsidiaries, employees, and
+        independent contractors to follow this Privacy Policy for the personal
+        information we share with them.
+      </li>
+      <li>
+        Third-Party Vendors: We may share information about you with third-party
+        vendors who need to know information about you to provide their services
+        to us, or to provide their services to you. This group includes vendors
+        that help us provide our Services to you (like payment providers that
+        process your credit and debit card information, or SMS and email
+        delivery services that help us stay in touch with you). We require
+        vendors to agree to privacy commitments to share information with them.
+        These vendors are listed in the "List of Data Sub-processors" section
+        below.
+      </li>
+      <li>
+        Legal Requests: We may disclose information about you in response to a
+        subpoena, court order, or other governmental requests.
+      </li>
+      <li>
+        To Protect the Rights, Property, and Others: We may disclose information
+        about you when we believe in good faith that disclosure is reasonably
+        necessary to protect the property or rights of {{ appName }}, third
+        parties, or the public. For example, if we have a good faith belief
+        there is an imminent danger of death or serious physical injury, we may
+        disclose information related to the emergency without delay.
+      </li>
+      <li>
+        With Your Consent: We may share and disclose information with your
+        consent or at your direction. For example, we may share your information
+        with third parties with which you authorize us to do so, such as the
+        notification services that you connect to your account on our site.
+      </li>
+    </ul>
+    <p class="text-h5">Security</p>
+    <p>
+      Although no online service is 100% secure, we work very hard to protect
+      information about you against unauthorized access, use, alteration, or
+      destruction, and take reasonable measures to do so, such as monitoring our
+      Services for potential vulnerabilities and attacks.
+    </p>
+    <p class="text-h5">Your Rights</p>
+    <p>
+      If you are located in certain countries, including those that fall under
+      the European General Data Protection Regulation (AKA the “GDPR”), data
+      protection laws give you rights regarding your personal data, subject to
+      any exemptions provided by the law, including the rights to:
+    </p>
+    <ul>
+      <li>Request access to your personal data.</li>
+      <li>Request correction or deletion of your personal data.</li>
+      <li>Object to our use and processing of your personal data.</li>
+      <li>
+        Request that we limit our use and processing of your personal data.
+      </li>
+      <li>Request portability of your personal data.</li>
+    </ul>
+    <p>
+      Usually, you can access, correct, or delete your personal data using your
+      account settings and tools that we offer, but if you aren't able to do
+      that, or you would like to contact us about one of the other rights,
+      please see "How To Contact Us" below for our contact details.
+    </p>
+    <p class="text-h5">How to Contact Us</p>
+    <p>
+      If you have a question about this Privacy Policy, or you would like to
+      contact us about any of the rights mentioned in the "Your Rights" section
+      above, please contact us:
+    </p>
+    <p>Email us at: {{ contactEmail }}</p>
+    <q-separator class="q-mb-md" />
+    <p>
+      This "Privacy Policy" is based on the "Creative Commons" licensed policy
+      by <a href="https://wordpress.com/">WordPress</a> ❤️.
+    </p>
   </q-page>
 </template>
 
 <script>
 export default {
-  // name: 'PageName',
-}
+  name: "PagePrivacyPolicy",
+  setup() {
+    return {
+      appName: process.env.VUE_APP_NAME,
+      systemUrl: process.env.VUE_APP_URL,
+      contactEmail: process.env.VUE_APP_CONTACT_EMAIL,
+    };
+  },
+};
 </script>
