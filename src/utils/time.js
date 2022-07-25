6fdc61b0cd6fff6ec283d2328056efd257acc5af
Albert Hidalgo <alhidalgodev@gmail.com>
2022-07-25 19:34:53 -0400

    feat
    * Created funcion to parse timestamp

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,10 +1,16 @@
 import dayjs from 'dayjs';
 import timezonePlugin from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 
+dayjs.extend(utc);
 dayjs.extend(timezonePlugin)
 
 const getUserTimezone = () => {
   return dayjs.tz.guess()
 }
 
-export { getUserTimezone }
+const getTimestampInHumanFormat = (timestamp, timezone) => {
+  return `${dayjs(timestamp).tz(timezone).format("MM/DD/YYYY")} at ${dayjs(timestamp).tz(timezone).format("HH:mm:ss")}`;
+}
+
+export { getUserTimezone, getTimestampInHumanFormat }
