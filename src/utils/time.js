f1d2e1f845857fc90b88b9984ccb556ab52141f3
Albert Hidalgo <alhidalgodev@gmail.com>
2022-08-08 20:31:22 -0400

    feat
    * Created duration method to get time ago with dayjs

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,9 +1,13 @@
 import dayjs from 'dayjs';
 import timezonePlugin from 'dayjs/plugin/timezone';
+import duration from 'dayjs/plugin/duration';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import utc from 'dayjs/plugin/utc';
 
 dayjs.extend(utc);
 dayjs.extend(timezonePlugin)
+dayjs.extend(duration)
+dayjs.extend(relativeTime)
 
 const getUserTimezone = () => {
   return dayjs.tz.guess()
@@ -13,4 +17,13 @@ const getTimestampInHumanFormat = (timestamp, timezone) => {
   return `${dayjs(timestamp).tz(timezone).format("MM/DD/YYYY")} at ${dayjs(timestamp).tz(timezone).format("HH:mm:ss")}`;
 }
 
-export { getUserTimezone, getTimestampInHumanFormat }
+const getDuration = (count) => {
+
+  if (count < 60) {
+    return dayjs.duration(count, "seconds").humanize();
+  } else if (count > 60 && count < 3600) {
+    return dayjs.duration(Math.floor(count / 60), "minutes").humanize();
+  }
+}
+
+export { getUserTimezone, getTimestampInHumanFormat, getDuration }
