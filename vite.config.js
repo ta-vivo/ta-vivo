b2f0901fbb0454ce2844b86f7c8db3b3ea6a5db2
Albert Hidalgo <alhidalgodev@gmail.com>
2022-10-06 19:36:57 -0400

    feat
    * Added unit testing configuration

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom'
+  },
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+    quasar({
+      sassVariables: 'src/quasar-variables.sass'
+    })
+  ],
+})
