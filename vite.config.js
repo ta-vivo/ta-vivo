3040d9d293f8b65d35bf432c74fa2df79e98b712
Albert Hidalgo <alhidalgodev@gmail.com>
2022-10-10 20:54:54 -0400

    feat
     * Added alias of the boot files

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ import path from 'path'
 export default defineConfig({
   resolve: {
     alias: {
-      'assets': path.resolve(__dirname, './src/assets')
+      'assets': path.resolve(__dirname, './src/assets'),
+      'boot': path.resolve(__dirname, './src/boot'),
     },
   },
   test: {
