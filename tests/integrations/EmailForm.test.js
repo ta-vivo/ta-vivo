7b73471a09659aa41875c89f7e8e499e3d2c103f
Albert Hidalgo <alhidalgodev@gmail.com>
2022-10-10 20:55:02 -0400

    feat
    * Added TODO

--- tests/integrations/EmailForm.test.js
+++ tests/integrations/EmailForm.test.js
@@ -1,12 +1,26 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 import { mount } from '@vue/test-utils'
 import { Quasar } from 'quasar'
+import { createStore } from 'vuex'
+
 
 import Email from "../../src/components/Integrations/Form/Email.vue";
 
+const actions = {
+  requestEmailCode: vi.fn(() => Promise.resolve()),
+}
+const mockStore = createStore({
+  modules: {
+    integrations: {
+      namespaced: true,
+      actions,
+    },
+  },
+})
+
 const wrapperFactory = () => mount(Email, {
   global: {
-    plugins: [Quasar],
+    plugins: [Quasar, mockStore],
     mocks: {
       $t: (msg) => msg
     }
@@ -20,11 +34,10 @@ describe('Email integration Form', () => {
     expect(Email).toBeTruthy();
   })
 
-  it('should render button to dispatch integration', async () => {
+  it('should test the request code flow', async () => {
     const integrationButton = wrapper.find('button');
     expect(integrationButton.exists()).toBe(true);
 
-    // find form and submit
     const form = wrapper.find('form');
     await form.trigger('submit');
 
@@ -33,6 +46,9 @@ describe('Email integration Form', () => {
 
     const input = wrapper.find('input');
     await input.setValue('jhon@doe.com');
+    await form.trigger('submit');
+
+    // TODO: Check if the requestEmailCode action was called
   })
 
 })
