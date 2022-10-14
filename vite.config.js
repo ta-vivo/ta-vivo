b5c285f53106fd7a31d4007b11bf8cf819077f76
Albert Hidalgo <alhidalgodev@gmail.com>
2022-10-13 21:14:01 -0400

    feat
    * Added alias

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import path from 'path'
 export default defineConfig({
   resolve: {
     alias: {
+      'src': path.resolve(__dirname, 'src'),
+      'components': path.resolve(__dirname, 'src/components'),
       'assets': path.resolve(__dirname, './src/assets'),
       'boot': path.resolve(__dirname, './src/boot'),
     },
