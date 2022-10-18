cb7a83bf002d23374451a331c6578d665bc6f314
Albert Hidalgo <alhidalgodev@gmail.com>
2022-10-18 19:13:37 -0400

    feat
    * Added more test for the auth store

--- tests/store/auth.test.js
+++ tests/store/auth.test.js
@@ -3,26 +3,31 @@ import { createStore } from 'vuex'
 
 import Auth from 'stores/Auth/index.js'
 
+const user = {
+  id: 1,
+  email: 'jhon@doe.com',
+  fullname: 'Jhon Doe',
+  active: true, enabled: true,
+  role: 'pro',
+  settings: {},
+  timezone: 'UTC'
+}
+
+const store = createStore({
+  modules: {
+    Auth
+  }
+});
+
 describe('Auth store', () => {
   it('should set user', () => {
-
-    const store = createStore({
-      modules: {
-        Auth
-      }
-    });
-
-    const user = {
-      id: 1,
-      email: 'jhon@doe.com',
-      fullname: 'Jhon Doe',
-      active: true, enabled: true,
-      role: 'pro',
-      settings: {},
-      timezone: 'UTC'
-    }
 
     store.commit('Auth/SET_USER', user);
     expect(store.state.Auth.user).toEqual(user);
   })
+
+  it('should get user', () => {
+    expect(store.getters['Auth/getUser']).toEqual(user);
+  })
+
 })
