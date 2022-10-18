a3439ad7728f307f7480b67f543619de3536d3b8
Albert Hidalgo <alhidalgodev@gmail.com>
2022-10-17 22:13:45 -0400

    feat
    * Added alias for store

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       'components': path.resolve(__dirname, 'src/components'),
       'assets': path.resolve(__dirname, './src/assets'),
       'boot': path.resolve(__dirname, './src/boot'),
+      'stores': path.resolve(__dirname, './src/store'),
     },
   },
   test: {
