1db01a922df51284028092e8cb86d635e9f7ec75
Albert Hidalgo <alhidalgodev@gmail.com>
2022-10-19 19:56:17 -0400

    refactor
    * Removed the export

--- tests/mocks/axios.js
+++ tests/mocks/axios.js
@@ -1,28 +1,24 @@
 import { vi } from 'vitest'
 
-const getMock = () => {
-  vi.mock('axios', () => ({
-    default: ({
-      create: () => ({
-        interceptors: {
-          request: { use: vi.fn(), eject: vi.fn() },
-          response: { use: vi.fn(), eject: vi.fn() }
-        },
-        get: vi.fn((route) => {
-          return Promise.resolve({ route: route, data: [] })
-        }),
-        post: vi.fn((route, data) => {
-          return Promise.resolve({ route: route, data: data })
-        }),
-        put: vi.fn((route, data) => {
-          return Promise.resolve({ route: route, data: data })
-        }),
-        delete: vi.fn((route) => {
-          return Promise.resolve({ route: route, data: [] })
-        })
+vi.mock('axios', () => ({
+  default: ({
+    create: () => ({
+      interceptors: {
+        request: { use: vi.fn(), eject: vi.fn() },
+        response: { use: vi.fn(), eject: vi.fn() }
+      },
+      get: vi.fn((route) => {
+        return Promise.resolve({ route: route, data: [] })
+      }),
+      post: vi.fn((route, data) => {
+        return Promise.resolve({ route: route, data: data })
+      }),
+      put: vi.fn((route, data) => {
+        return Promise.resolve({ route: route, data: data })
+      }),
+      delete: vi.fn((route) => {
+        return Promise.resolve({ route: route, data: [] })
       })
     })
-  }))
-}
-
-export default getMock;
+  })
+}))
