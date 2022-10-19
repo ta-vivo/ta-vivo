564a96e50124f58b672e2d2576a6092d8e4d3008
Albert Hidalgo <alhidalgodev@gmail.com>
2022-10-19 19:50:46 -0400

    feat
    * Added more method to the checks actions test

--- tests/mocks/axios.js
+++ tests/mocks/axios.js
@@ -1,13 +1,28 @@
 import { vi } from 'vitest'
 
-vi.mock('axios', () => ({
-  default: ({
-    create: () => ({
-      interceptors: {
-        request: { use: vi.fn(), eject: vi.fn() },
-        response: { use: vi.fn(), eject: vi.fn() }
-      },
-      get: vi.fn()
+const getMock = () => {
+  vi.mock('axios', () => ({
+    default: ({
+      create: () => ({
+        interceptors: {
+          request: { use: vi.fn(), eject: vi.fn() },
+          response: { use: vi.fn(), eject: vi.fn() }
+        },
+        get: vi.fn((route) => {
+          return Promise.resolve({ route: route, data: [] })
+        }),
+        post: vi.fn((route, data) => {
+          return Promise.resolve({ route: route, data: data })
+        }),
+        put: vi.fn((route, data) => {
+          return Promise.resolve({ route: route, data: data })
+        }),
+        delete: vi.fn((route) => {
+          return Promise.resolve({ route: route, data: [] })
+        })
+      })
     })
-  })
-}))
+  }))
+}
+
+export default getMock;
