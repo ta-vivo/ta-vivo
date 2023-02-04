11c38abbf4ecfaa2fa686b5a1da58397a2ea7154
Albert Hidalgo <alhidalgodev@gmail.com>
2023-02-04 11:51:27 -0400

    feat
    * Created action to send the status page creation

--- src/store/StatusPages/actions.js
+++ src/store/StatusPages/actions.js
@@ -3,3 +3,7 @@ import { axiosInstance } from 'boot/axios'
 export function fetchAll(_, queryString) {
   return axiosInstance.get(`/status-pages${queryString || ''}`)
 }
+
+export function create(_, payload) {
+  return axiosInstance.post('/status-pages', payload)
+}
