e6cc14d5effae95e5669121cfdd069aabd0d718a
Albert Hidalgo <alhidalgodev@gmail.com>
2023-02-06 21:52:36 -0400

    feat
    * Created the action to fetch status page by uuid

--- src/store/StatusPages/actions.js
+++ src/store/StatusPages/actions.js
@@ -4,6 +4,10 @@ export function fetchAll(_, queryString) {
   return axiosInstance.get(`/status-pages${queryString || ''}`)
 }
 
+export function fetchByuuid(_, uuid) {
+  return axiosInstance.get(`/status-pages/${uuid}`)
+}
+
 export function create(_, payload) {
   return axiosInstance.post('/status-pages', payload)
 }
