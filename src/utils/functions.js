ef139b0b4cf2c188789b54e2abfa43eec3f72e32
Albert Hidalgo <alhidalgodev@gmail.com>
2023-02-11 21:14:10 -0400

    feat
    * Created function util

---
+++ src/utils/functions.js
@@ -0,0 +1,25 @@
+const getMsOrSecondsFromMs = (ms) => {
+  if (ms < 1000) {
+    return `${ms} ms`;
+  }
+  return `${(ms / 1000).toFixed(2)} ${$t("common.seconds")}`;
+};
+
+const getTheAverageResponseTime = (data) => {
+  const averageTime = data.reduce(
+    (acc, log) => acc + parseFloat(log.duration),
+    0
+  );
+  return data.length > 0
+    ? getMsOrSecondsFromMs(averageTime / data.length)
+    : 0;
+};
+
+const getUpTimePercent = (data) => {
+  const upTime = data.filter((log) => log.status === "up").length;
+  return data.length > 0
+    ? `${((upTime / data.length) * 100).toFixed(0)}%`
+    : "0.00%";
+};
+
+export { getMsOrSecondsFromMs, getTheAverageResponseTime, getUpTimePercent }
