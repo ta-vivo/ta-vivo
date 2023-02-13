d7017eaf68ebbb8ce81fd68eafab26775161e9e0
Albert Hidalgo <alhidalgodev@gmail.com>
2023-02-13 19:10:52 -0400

    feat
    * Created 404 component

---
+++ src/components/Interface/404.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-white text-center q-pa-md flex flex-center">
+    <div>
+      <div style="font-size: 30vh">404</div>
+
+      <div class="text-h2" style="opacity: 0.4">
+        {{ $t("messages.errors.nothingHere") }}
+      </div>
+
+      <q-btn
+        push
+        class="q-mt-xl"
+        color="primary"
+        to="/"
+        :label="$t('action.goHome')"
+      />
+    </div>
+  </div>
+</template>
