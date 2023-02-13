cdf942a917faf93eeb4532f30994fa3bc006364e
Albert Hidalgo <alhidalgodev@gmail.com>
2023-02-13 19:29:01 -0400

    feat
    * Created dark mode toggle component

---
+++ src/components/Interface/DarkModeToggle.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-toggle
+    color="primary"
+    dark
+    v-model="darkMode"
+    :icon="darkMode ? 'eva-moon-outline' : 'eva-sun-outline'"
+    @update:model-value="toggleDarkMode"
+  />
+</template>
+
+<script>
+import { useQuasar } from "quasar";
+import { ref } from "vue";
+export default {
+  name: "ComponentDarkModeToggle",
+  setup() {
+    const $q = useQuasar();
+
+    const darkmodeFromLocalStorage = localStorage.getItem("darkmode");
+    const darkMode = ref(darkmodeFromLocalStorage || $q.dark.isActive);
+
+    return {
+      darkMode,
+      toggleDarkMode() {
+        window.localStorage.setItem("darkMode", darkMode.value);
+        $q.dark.set(darkMode.value);
+      },
+    };
+  },
+};
+</script>
