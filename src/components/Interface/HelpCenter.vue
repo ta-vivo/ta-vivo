55ee0cfbad2b282566e319a730a8397b3bacf996
Albert Hidalgo <alhidalgodev@gmail.com>
2023-05-21 16:54:21 -0400

    feat
    * Added the initial video

--- src/components/Interface/HelpCenter.vue
+++ src/components/Interface/HelpCenter.vue
@@ -10,8 +10,35 @@
     </q-page-sticky>
 
     <q-dialog v-model="showHelpCenter" position="right">
-      <q-card style="max-width: 400px">
-        <q-card-section class="row items-center bg-primary text-white">
+      <q-card style="min-width: 400px; min-height: 500px">
+        <q-card-section
+          v-show="showVideo"
+          class="row items-center bg-primary text-white"
+        >
+          <q-btn
+            class="q-mr-md"
+            @click="() => (showVideo = false)"
+            icon="eva-chevron-left-outline"
+            flat
+            :label="$t('action.back')"
+          />
+          <div class="text-h6">
+            {{ selectedOption.label }}
+          </div>
+        </q-card-section>
+        <q-card-section v-show="showVideo">
+          <video
+            style="width: 600px; height: auto"
+            controls
+            autoplay
+            :src="selectedOption.video"
+          ></video>
+        </q-card-section>
+
+        <q-card-section
+          v-show="!showVideo"
+          class="row items-center bg-primary text-white"
+        >
           <div class="text-h6">{{ $t("messages.helpCenter.welcome") }}</div>
           <q-space />
           <q-btn
@@ -23,11 +50,11 @@
           />
 
           <div>
-            {{ $t('messages.helpCenter.description') }}
+            {{ $t("messages.helpCenter.description") }}
           </div>
         </q-card-section>
 
-        <q-card-section>
+        <q-card-section v-show="!showVideo">
           <q-list bordered class="rounded-borders">
             <q-expansion-item
               v-for="(item, index) in menuItems"
@@ -43,6 +70,7 @@
                   v-for="(subItem, subIndex) in item.subItems"
                   :key="subIndex"
                   clickable
+                  @click="() => handleClickOnOption(subItem)"
                 >
                   <q-item-section>
                     <q-item-label>- {{ subItem.label }}</q-item-label>
@@ -66,11 +94,13 @@ export default {
   setup() {
     const $t = useI18n().t;
 
-    const showHelpCenter = ref(false);
+    const showHelpCenter = ref(true);
+    const showVideo = ref(false);
+    const selectedOption = ref({});
     const menuItems = ref([
       {
         label: $t("common.getting_started"),
-        icon: 'eva-compass-outline',
+        icon: "eva-compass-outline",
         subItems: [
           { label: $t("common.overview_tool") },
           { label: $t("common.quick_setup") },
@@ -78,16 +108,19 @@ export default {
       },
       {
         label: $t("common.creating_checks"),
-        icon: 'eva-activity-outline',
+        icon: "eva-activity-outline",
         subItems: [
-          { label: $t("common.setting_targets") },
+          {
+            label: $t("common.setting_targets"),
+            video: "https://tavivo.s3.amazonaws.com/create+checks.mp4",
+          },
           { label: $t("common.configuring_intervals") },
           { label: $t("common.specifying_timezone") },
         ],
       },
       {
         label: $t("common.integrations"),
-        icon: 'eva-briefcase-outline',
+        icon: "eva-briefcase-outline",
         subItems: [
           { label: $t("common.discord_integration") },
           { label: $t("common.email_notifications") },
@@ -99,7 +132,7 @@ export default {
       },
       {
         label: $t("common.status_pages"),
-        icon: 'eva-bar-chart-outline',
+        icon: "eva-bar-chart-outline",
         subItems: [
           { label: $t("common.create_dashboard") },
           { label: $t("common.share_information") },
@@ -107,21 +140,17 @@ export default {
       },
       {
         label: $t("common.log_history"),
-        icon: 'eva-file-text-outline',
-        subItems: [
-          { label: $t("common.tracking_uptime") },
-        ],
+        icon: "eva-file-text-outline",
+        subItems: [{ label: $t("common.tracking_uptime") }],
       },
       {
         label: $t("common.timezone_configuration"),
-        icon: 'eva-clock-outline',
-        subItems: [
-          { label: $t("common.managing_general_timezones") },
-        ],
+        icon: "eva-clock-outline",
+        subItems: [{ label: $t("common.managing_general_timezones") }],
       },
       {
         label: $t("common.faqs"),
-        icon: 'eva-question-mark-circle-outline',
+        icon: "eva-question-mark-circle-outline",
         subItems: [
           { label: $t("common.frequently_asked_questions") },
           { label: $t("common.troubleshooting_issues") },
@@ -129,7 +158,7 @@ export default {
       },
       {
         label: $t("common.contact_support"),
-        icon: 'eva-headphones-outline',
+        icon: "eva-headphones-outline",
         subItems: [
           { label: $t("common.reach_out") },
           { label: $t("common.feedback_requests") },
@@ -140,8 +169,16 @@ export default {
     return {
       menuItems,
       showHelpCenter,
+      showVideo,
+      selectedOption,
       handleShowHelpCenter: () => {
         showHelpCenter.value = true;
+      },
+      handleClickOnOption: (option) => {
+        if (option.video) {
+          selectedOption.value = option;
+          showVideo.value = true;
+        }
       },
     };
   },
