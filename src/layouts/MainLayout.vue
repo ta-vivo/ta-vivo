d48978d239e2b6f2da8e439351febc446012007f
Albert Hidalgo <alhidalgodev@gmail.com>
2023-05-21 11:24:37 -0400

    feat
    * Added the help center

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,12 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header :class="$q.dark.isActive ? 'bg-dark' : 'bg-white'">
-      <MobileMenu v-if="$q.screen.xs"/>
-      <DesktopMenu v-else/>
+      <MobileMenu v-if="$q.screen.xs" />
+      <DesktopMenu v-else />
     </q-header>
     <q-page-container>
       <router-view :key="$route.fullPath" />
+      <help-center />
     </q-page-container>
   </q-layout>
 </template>
@@ -14,15 +15,17 @@
 import { defineComponent, ref } from "vue";
 import DesktopMenu from "src/components/Interface/DesktopMenu.vue";
 import MobileMenu from "src/components/Interface/MobileMenu.vue";
+import HelpCenter from "components/Interface/HelpCenter";
 
 export default defineComponent({
   name: "MainLayout",
   components: {
     DesktopMenu,
-    MobileMenu
+    MobileMenu,
+    HelpCenter,
   },
   setup() {
-   return {}
+    return {};
   },
 });
 </script>
